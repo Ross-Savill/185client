d86ed5ebff843d0e85531906586083f2bdca6788
Callum Thompson <callumrthompson@me.com>
2019-01-22 11:39:11 +1100

    implemented login form and react router

---
+++ client/src/Components/LoginForm.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import '../Styles/Login.css';
+import axios from 'axios';
+
+class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      error: ""
+    }
+  }
+
+  inputChange = (e) => {
+    const { id, value } = e.currentTarget
+    this.setState({ [id]:value })
+  }
+  
+  login = (e) => {
+    e.preventDefault()
+    console.log("Login Button Pressed")
+    // axios.post the username and password to authentication path on our API
+  }
+
+  render() {
+    return (
+      <>
+        <div id="form-container">
+          <div id="login-form">
+            <h2>185 Warehouse Dashboard</h2>
+            <p>Please enter your details below to log in</p>
+            <hr />
+            {this.state.error && <span className="error">{this.state.error}</span>}
+            <form>
+              <p>
+                <label htmlFor="email">Email: </label>
+                <input type="text" id="email" onChange={this.inputChange}></input><br />
+              </p>
+              <p>
+                <label htmlFor="password">Password: </label>
+                <input type="password" id="password" onChange={this.inputChange}></input><br />
+              </p>
+              <button id="login" onClick={this.login}>Log In</button>
+            </form>
+            {/* <p>
+              <span>Email: {this.state.email}</span> <br/>
+              <span>Password: {this.state.password}</span>
+            </p> */}
+          </div>
+        </div>
+      </>
+    )
+  }
+}
+
+export default LoginForm;
