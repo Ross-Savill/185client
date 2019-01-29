04f3ec3c886b20c7613911dde6350221a4d434ab
Ross Savill <40708796+Ross-Savill@users.noreply.github.com>
2019-01-29 15:05:13 +1100

    Delete index.js

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/', require('./public'));
-router.use('/auth', require('./auth'));
-router.use('/inventory', require('./inventory'));
-router.use('/orders', require('./orders'));
-
-module.exports = router;
