a9de61c43300c3c4b94db15e1588285a1c1f9969
Callum Thompson <callumrthompson@me.com>
2019-01-31 13:59:45 +1100

    updated authentication redirects

--- client/src/Components/Sidebar.js
+++ client/src/Components/Sidebar.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import '../Styles/App.css';
+import { BrowserRouter as Router, Route, Redirect, Link } from "react-router-dom";
 
 class Sidebar extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      loggedOut: false
     }
   }
 
   logout = () => {
-    localStorage.removeItem('token')
+    localStorage.removeItem('token');
+    this.setState({loggedOut: true})
   }
 
   render() {
+    if(!this.state.loggedOut){
     return (
       <>
         <div id="sidebar">
@@ -24,39 +28,42 @@ class Sidebar extends Component {
             <ul>
               <li>Stock</li>
               <hr />
-              <li>Total Inventory</li>
-              <li>Add Inbound Stock</li>
-              <li>Alerts (3)</li>
+              <li><Link to="/dashboard/inventory">Total Inventory</Link></li>
+              <li><Link to="/dashboard/inventory/add">Add Inbound Stock</Link></li>
+              <li><Link to="/dashboard/inventory/alerts">Alerts (3)</Link></li>
             </ul>
             <ul>
               <li>Products</li>
               <hr />
-              <li>View All</li>
-              <li>Add</li>
-              <li>Delete</li>
-              <li>Edit</li>
+              <li><Link to="/dashboard/products">View All</Link></li>
+              <li><Link to="/dashboard/products/add">Add</Link></li>
+              <li><Link to="/dashboard/products/delete">Delete</Link></li>
+              <li><Link to="/dashboard/products/edit">Edit</Link></li>
             </ul>
             <ul>
               <li>Orders</li>
               <hr />
-              <li>View All</li>
-              <li>Add</li>
-              <li>Delete</li>
-              <li>Edit</li>
+              <li><Link to="/dashboard/orders">View All</Link></li>
+              <li><Link to="/dashboard/orders/add">Add</Link></li>
+              <li><Link to="/dashboard/orders/delete">Delete</Link></li>
+              <li><Link to="/dashboard/orders/edit">Edit</Link></li>
             </ul>
             <ul>
               <li>Users</li>
               <hr />
-              <li>View All</li>
-              <li>Add</li>
-              <li>Delete</li>
-              <li>Edit</li>
+              <li><Link to="/dashboard/users">View All</Link></li>
+              <li><Link to="/dashboard/users/add">Add</Link></li>
+              <li><Link to="/dashboard/users/delete">Delete</Link></li>
+              <li><Link to="/dashboard/users/edit">Edit</Link></li>
             </ul>
           </div>
         </div>
       </>
     )
+  } else {
+    return <Redirect to="/login" />
   }
+}
 }
 
 export default Sidebar;
