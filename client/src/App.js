e577a16edca8ebd78ef833e2e752c70612aa91d7
Steve <admin@cryptostat.org>
2019-02-07 14:26:21 +1100

    user buttons, order functions

--- client/src/App.js
+++ client/src/App.js
@@ -20,22 +20,14 @@ class App extends Component {
         <>
           <Route path="/login" exact component={LoginForm} />
           <Route path="/register" component={RegisterForm} />
-
           <Route path="/" exact component={Dashboard} />
           <Route path="/dashboard" component={Dashboard} />
           <Route path="/inventory" component={Inventory} />
-
           <Route path="/dashboard/inventory" component={Inventory} />
-
-          {/* <Route path="/dashboard/orders" component= /> */}
           <Route path="/dashboard/orders/add" component={NewOrder}/>
-          {/* <Route path="/dashboard/orders/delete" component= />
-          <Route path="/dashboard/orders/edit" component= /> */}
-
           <Route path="/dashboard/users" exact component={Users}/>
-
-          <Route path="/dashboard/orders/:orderID/pick" component={PickList} />
           <Route path="/dashboard/orders/" exact component={Orders}/>
+          <Route path="/dashboard/orders/:orderID/pick" component={PickList} />
           <Route path="/neworder" component={NewOrder} />
         </>
       </Router>
--- client/src/Components/Users.js
+++ client/src/Components/Users.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 import '../Styles/App.css';
 import axios from 'axios';
-import { Redirect } from "react-router-dom";
+import {ReactDOM} from "react-dom";
+import {Redirect} from "react-router-dom"
+import MUIDataTable from "mui-datatables";
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import Badge from '@material-ui/core/Badge';
 
 class Users extends Component {
   constructor(props) {
@@ -10,23 +24,26 @@ class Users extends Component {
       users: null,
       message: null,
       username: "",
-      passport: "",
-      role: "",
-      addError: null
+      password: "",
+      role: "user",
+      addError: null,
+      showEdit: false,
+      openEdit: true,
+      user: {
+        username: "",
+        password: "",
+        _id: 0
+      },
+      showAlert: false,
+      alertMessageText: "",
     }
   }
 
   componentDidMount() {
-    const url = 'https://vast-earth-81912.herokuapp.com/auth/users'
-    axios.get(url)
-      .then(res => {
-        const users = res.data.data
-        const message = res.data.message
-        this.setState({ users, message })
-      })
+    this.loadDatabase()
   }
 
-  componentDidUpdate() {
+  loadDatabase = () => {
     const url = 'https://vast-earth-81912.herokuapp.com/auth/users'
     axios.get(url)
       .then(res => {
@@ -40,7 +57,7 @@ class Users extends Component {
     const userArray = this.state.users
     if(userArray){
       return (
-        userArray.map((user, i) => <li><strong>{user.username}</strong> ({user.role}) <button id="edit" data-user={user._id}>Edit</button> {i != 0 ? <button id={user._id} onClick={this.deleteUser}>Delete</button>:null}</li>)
+        userArray.map((user, i) => <li><strong>{user.username}</strong> ({user.role}) <button className="btn btn-primary" id="edit" onClick={ () => {this.openEdit(user)}} data-user={user._id}>Edit</button> {i != 0 ? <button className="btn btn-primary" id={user._id} onClick={this.deleteUser}>Delete</button>:null}</li>)
       )}
   }
 
@@ -52,7 +69,7 @@ class Users extends Component {
 
   checkboxClick = (e) => {
     if(this.state.role === 'admin') {
-      this.setState({ role: null })
+      this.setState({ role: 'user' })
     } else {
       this.setState({ role: 'admin' })
     }
@@ -60,11 +77,11 @@ class Users extends Component {
 
   addUser = (e) => {
     e.preventDefault()
-    const { username, password, admin} = this.state
+    const { username, password, role} = this.state
     axios.post('https://vast-earth-81912.herokuapp.com/auth/register', {
-      username: username,
-      password: password,
-      role: admin
+      username,
+      password,
+      role
     })
       .then(res => {
         this.setState({addError: res.data.message})
@@ -77,9 +94,97 @@ class Users extends Component {
     axios.delete('https://vast-earth-81912.herokuapp.com/auth/users', {data: {'_id': id}})
   }
 
+  changePassword = (e) => {
+    e.preventDefault()
+    const {id, password} = e.currentTarget
+    axios.put('https://vast-earth-81912.herokuapp.com/auth/users', {data: {'_id': id, password}})
+  }
+
+  openEdit = (user) => {
+    user.password=""
+    this.setState({showEdit: true, user})
+
+  }
+
+  closeEdit = () => {
+    this.setState({showEdit: false})
+  }
+
+  submitEdit = () => {
+    const { user } = this.state;
+    axios.put('https://vast-earth-81912.herokuapp.com/auth/users', {
+      username: user.username,
+      password: user.password
+    })
+    .then(response => {
+      if(response.data.success) {
+        this.setState({showAlert:true, alertMessageText:response.data.message, showEdit:false})
+      } else {
+        this.setState({showAlert:true, alertMessageText:response.data.message})
+
+      }
+    })
+  }
+
+  messageBoxClose= () => {
+    this.setState({showAlert:false})
+  }
+
+  messageBox = () => {
+    return (
+    <Snackbar anchorOrigin={{vertical: 'top', horizontal: 'right',}}
+      open={this.state.showAlert}
+      autoHideDuration={5000}
+      onClose={this.messageBoxClose}
+      message={<span id="message-id">{this.state.alertMessageText}</span>}
+      />
+    )
+  }
+
+  messageBoxOpen = () => {
+    this.setState({alertMessage:true})
+  }
+
+
+  inputChangeEdit = (e) => {
+      const { value } = e.currentTarget
+      const { user } = this.state
+      user.password = value
+      console.log(user.password)
+      console.log(user);
+      this.setState({user})
+    }
+
+  editUser = () => {
+    const { user } = this.state;
+    console.log(user)
+    return (
+    <Dialog open={this.state.showEdit} onClose={this.closeEdit} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+          <DialogTitle id="alert-dialog-title"> </DialogTitle>
+          <DialogContent>
+            Change password for '{user.username}'
+            <br/>
+            <label htmlFor="password">Password: </label>
+            <input type="text" id="password" onChange={this.inputChangeEdit}></input><br />
+
+          </DialogContent>
+          <DialogActions>
+            <button className="btn btn-outline-primary btn-sm" onClick={this.closeEdit}>
+              Close
+            </button>
+            <button className="btn btn-outline-primary btn-sm" onClick={this.submitEdit} autoFocus>
+              Save
+            </button>
+          </DialogActions>
+        </Dialog>
+        )
+  }
+
   render() {
     return (
       <>
+        {this.editUser()}
+        {this.messageBox()}
         <div id="crud-container">
           <h2>Users</h2>
           <hr />
